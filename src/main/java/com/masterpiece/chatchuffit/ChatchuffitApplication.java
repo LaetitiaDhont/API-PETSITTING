d4c8c03973c62c1199c20471d9328ae8d7c0641b
Dhont Laetitia <laetitiadhont75@gmail.com>
2019-07-17 09:50:02 +0200

    Initial Commit

---
+++ src/main/java/com/masterpiece/chatchuffit/ChatchuffitApplication.java
@@ -0,0 +1,13 @@
+package com.masterpiece.chatchuffit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatchuffitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatchuffitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/masterpiece/chatchuffit/ChatchuffitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.masterpiece.chatchuffit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChatchuffitApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
